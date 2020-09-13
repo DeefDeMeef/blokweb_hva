9aa038c37980b6183eacb75417ef1a80bde60e59
DaveyZaal <30734726+DaveyZaal@users.noreply.github.com>
2020-09-13 22:58:30 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+// sidebar js
+var sidebar = document.getElementById('sidebar')
+var hamburger_icon = document.getElementById('open-sidebar')
+var close_sidebar = document.getElementById('close-sidebar')
+
+function openSidebar() {
+    sidebar.style.height = '100%'
+}
+
+function closeSidebar() {
+    sidebar.style.height = '0'
+}
+
+hamburger_icon.onclick = openSidebar
+close_sidebar.onclick = closeSidebar
+
+// einde sidebar js
