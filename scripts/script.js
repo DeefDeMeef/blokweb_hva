d83c9f485ad474038ff17fdc64cdb1c3f03b089f
DaveyZaal <30734726+DaveyZaal@users.noreply.github.com>
2020-10-09 10:19:43 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,31 @@
-// sidebar js
+let root = document.documentElement
+
+// start sidebar 
 var sidebar = document.getElementById('sidebar')
 var hamburger_icon = document.getElementById('open-sidebar')
 var close_sidebar = document.getElementById('close-sidebar')
 
 function openSidebar() {
-    sidebar.style.height = '100%'
+    root.style.setProperty('--height_open', '100%')
 }
 
 function closeSidebar() {
-    sidebar.style.height = '0'
+    root.style.setProperty('--height_open', '0')
 }
 
 hamburger_icon.onclick = openSidebar
 close_sidebar.onclick = closeSidebar
+// end sidebar
 
-// end sidebar js
-
-const init = function(){
-	let items = document.querySelectorAll('section');
-	for (let i = 0; i < items.length; i++){
-		items[i].style.background = randomColor({luminosity: 'light'});
-	}
-	cssScrollSnapPolyfill()
+// start loader
+function hideLoader() {
+	root.style.setProperty('--loader', 0)
+	root.style.setProperty('--display', 'none');
+	console.log('page loaded')
 }
-init();
+
+window.onload = (event) => {
+	setTimeout(hideLoader, 500)
+	console.log("page loading...")
+}
+// end loader
